a427c330754d2f8c4d55948a0aef51df94fe6361
wilyos <wil99@gmail.com>
2023-04-19 20:39:37 -0500

    buttom-tab, materialicon and react-native-paper

--- App.js
+++ App.js
@@ -1,54 +1,96 @@
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ProfileScreen from './components/ProfileScreen';
-import Login from './components/login';
-const Stack = createNativeStackNavigator()
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {MaterialIcons} from '@expo/vector-icons'
+import { TextInput, Button, Icon } from 'react-native-paper';
+import { useState } from 'react';
+
+let users = [
+{ username: 'wilyos', password: '1234', fullname: 'Wilmer B', role: 1 }, 
+{ username: 'babayagga', password: '666', fullname: 'Jhon Whick', role: 2 }
+]
+
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
 
 export default function App() {
   return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='Login'>
-          <Stack.Screen name="Home" component={HomeScreen}options={{title:'Inicio'}} />
-          <Stack.Screen name="Contacts" component={ContacScreen} options={{title:'Contactos'}}/>
-          <Stack.Screen name="Profile" component={ProfileScreen} options={{title:'Perfil del Usuario'}} />
-          <Stack.Screen name="Login" component={Login} options={{title:'Login'}} />
+        <Stack.Navigator initialRouteName='HomeTabs'>
+
+          <Stack.Screen name="HomeTabs" component={HomeTabs}options={{title:'Sistema de prueba'}} />
+         
         </Stack.Navigator>
       </NavigationContainer>
   );
 }
-function HomeScreen({route,navigation}){
+function LoginScreen({route,navigation}){
+  const [username,setUsername] = useState('')
+  const [password,setPassword] = useState('')
+  const [errormessage, setErrorMessage] = useState('')
   return(
     <View style={styles.container}>
-      <Text>Inicio</Text>
-      <Text>Bienvenido: {route.params.fullname}</Text>
-      <Button 
-      title='Ir a contáctenos'
-      onPress={()=>{
-        navigation.navigate('Contacts')
-      }}
-      />
+      <Text style={{marginBottom:10}} >Inicio</Text>
+      <Text style={{color:'red'}} >{errormessage}</Text>
+      <TextInput label="Usuario" mode='outlined' left={<TextInput.Icon icon="account"/>} onChangeText={username=> setUsername(username)} value={username} />
+
+      <TextInput style={{marginTop:10,marginBottom:10}} label="Contraseña" mode='outlined' left={<TextInput.Icon icon="eye" />} onChangeText={password => setPassword(password)} value={password} secureTextEntry/>
+      
+      <Button icon="door" mode="contained" onPress={()=>{
+        let login = users.find(person => person.username == username && person.password)
+        if(login != undefined){
+          const {fullname, username} = login
+          setErrorMessage('')
+          navigation.navigate('Contacts',{name:fullname,username:username})
+        }
+        else{
+          setErrorMessage('Usuario y/o contraseña invalidos')
+        }
+      }}>
+        Sign in
+      </Button>
     </View>
+    
   )
 }
 
-function ContacScreen({navigation}){
+function ContacScreen({route,navigation}){
   let fullname = 'Jhon Whick'
   let username = 'babayaga'
   let salary = 2500000
   return(
     <View style={styles.container}>
       <Text>Estamos en Contact</Text>
-      <Button 
-      title='Perfil del usuario'
-      onPress={()=>{
-        navigation.navigate('Profile',{fullname: fullname, username: username,salary:salary})
-      }}
-      />
+      <Text>{route.params.name}</Text>
+      <Text>{route.params.username}</Text>
     </View>
   )
 }
 
+function HomeTabs(){
+  return(
+    <Tab.Navigator
+      screenOptions={{tabBarActiveTinColor:'red', tabBarInactiveBackgroundColor:'white', tabBarActiveBackgroundColor:'orange', headerShown:false}}
+      >
+      <Tab.Screen name="Login" component={LoginScreen} options={{
+        tabBarStyle:{display:'none'},
+        tabBarIcon:(tabInfo)=>(<MaterialIcons name="person" size={25}/>)
+      }}/>
+      <Tab.Screen name="Contacts" component={ContacScreen} options={{
+        tabBarIcon: (tabInfo) => (<MaterialIcons name="contacts" size={25} />)
+      }} />
+      <Tab.Screen name="Profile" component={ProfileScreen} options={{
+        //tabBarStyle:{display:'none'}
+        tabBarIcon: (tabInfo) => (<MaterialIcons name="zoom-out" size={25} />)
+      }}/>
+    </Tab.Navigator>
+  )
+
+}
 
 
 
--- components/ProfileScreen.jsx
+++ components/ProfileScreen.jsx
@@ -5,10 +5,11 @@ function ProfileScreen ({route,navigation}){
   
     return(
       <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+       {/* <Text>Estamos en el perfil de usuario</Text>
+        <Text>Nombre completo es: </Text>
+        <Text>El salario: </Text>
+    <TextInput value={''}/> */}
         <Text>Estamos en el perfil de usuario</Text>
-        <Text>Nombre completo es: {route.params.fullname}</Text>
-        <Text>El salario: {route.params.salary}</Text>
-        <TextInput value={route.params.username}/>
       </View>
     )
   }
